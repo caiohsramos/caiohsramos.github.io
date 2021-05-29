9835872d65dab5c023381b58fe16cb3346e925b8
Caio Ramos <caioramos97@gmail.com>
2021-05-29 01:58:40 -0300

    start working in /blog

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 import Main from "./containers/Main";
+import BlogPost from "./containers/blogPost/BlogPost";
 
 function App() {
   return (
-    <div>
-      <Main />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route path="/blog/:id" component={BlogPost} />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/containers/blogPost/BlogPost.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react"
+import ReactMarkdown from 'react-markdown'
+import raw from "raw.macro"
+import 'github-markdown-css'
+import { useHistory } from "react-router-dom";
+import "./BlogPost.css"
+
+export default ({ match: { params: { id } } }) => {
+  const [mdText, setMdText] = useState("")
+  const history = useHistory()
+  useEffect(() => {
+    const blogPostMd = raw(`../../blog/${id}.md`)
+    if (id && !blogPostMd) history.replace("/")
+    setMdText(blogPostMd)
+  }, [id, history])
+  return (
+    <ReactMarkdown>{mdText}</ReactMarkdown>
+  )
+}
