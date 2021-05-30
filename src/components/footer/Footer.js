f32b7c59c875edb00df6c5c2a172b8e53edbde69
Caio Ramos <caioramos97@gmail.com>
2021-05-30 20:55:39 -0300

    refactor footer to use styled components

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,20 +1,31 @@
-import React, { useContext } from "react";
-import "./Footer.css";
-import { Fade } from "react-reveal";
+import React, {useContext} from "react";
+import styled from "styled-components";
+import {Fade} from "react-reveal";
 import StyleContext from "../../contexts/StyleContext";
 
+const StyledDiv = styled.div`
+  margin-top: 2rem;
+  padding-bottom: 1rem;
+`;
+
+const StyledText = styled.p`
+  text-align: center;
+  color: ${props =>
+    props.isDark ? "color: white !important;" : "#868e96 !important;"};
+`;
+
 export default function Footer() {
-  const { isDark } = useContext(StyleContext);
+  const {isDark} = useContext(StyleContext);
   return (
     <Fade bottom duration={1000} distance="5px">
-      <div className="footer-div">
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+      <StyledDiv>
+        <StyledText isDark={isDark}>
           Theme by{" "}
           <a href="https://github.com/saadpasta/developerFolio">
             developerFolio
           </a>
-        </p>
-      </div>
+        </StyledText>
+      </StyledDiv>
     </Fade>
   );
 }
